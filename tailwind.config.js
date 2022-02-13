43ae5e06ef2e65e0317336ae5dabcc69231ecc43
priyanshkulshrestha <priyanshkulshrestha13@gmil.com>
2022-02-14 03:11:03 +0530

    almost complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   "./components/**/*.{js,ts,jsx,tsx}"],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      "3xl": "2000px",
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
